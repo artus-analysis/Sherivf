bf975d55c1ff11bef7f1d70c1d78feab0cb82c57
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-03-24 17:26:52 +0100

    ini

---
+++ MCgrid_CMS_2015_Zee.cc
@@ -0,0 +1,150 @@
+// -*- C++ -*-
+#include "Rivet/Analysis.hh"
+#include "Rivet/Projections/FinalState.hh"
+#include "Rivet/Projections/ZFinder.hh"
+
+#include "mcgrid/mcgrid.hh"
+
+namespace Rivet {
+
+  /// @brief CDF Z boson rapidity measurement
+  /// modified to generate APPLgrid or fastNLO files
+  class MCgrid_CMS_2015_Zee : public Analysis {
+  public:
+
+    /// @name Constructors etc.
+    //@{
+
+    /// Constructor
+    MCgrid_CMS_2015_Zee()
+      : Analysis("MCgrid_CMS_2015_Zee")
+    {    }
+
+    //@}
+
+
+  public:
+
+    /// @name Analysis methods
+    //@{
+
+    /// Book histograms and initialise projections before the run
+    void init() {
+
+      /// Initialise and register projections here
+      // this seems to have been corrected completely for all selection cuts,
+      // i.e. eta cuts and pT cuts on leptons.
+      ZFinder zfinder(FinalState(), Cuts::open(), PID::ELECTRON,
+                      66*GeV, 116*GeV, 0.2, ZFinder::CLUSTERNODECAY, ZFinder::TRACK);
+      addProjection(zfinder, "ZFinder");
+      
+      /// Book histograms here
+      _h_xs = bookHisto1D(1, 1, 1);
+      _h_yZ = bookHisto1D(2, 1, 1);
+      
+#if USE_APPL
+	  MSG_INFO("Using applgrid");
+      const string PDFname = "MCgrid_CDF_2009_S8383952.config";
+      MCgrid::bookPDF(PDFname, histoDir(), MCgrid::BEAM_PROTON, MCgrid::BEAM_ANTIPROTON);
+      MCgrid::applGridArch arch_appl(50,1,5,0);
+      MCgrid::applGridConfig config_appl(0, PDFname, 1E-5, 1, 8315.18, 8315.18, arch_appl);
+      _appl_yZ = MCgrid::bookGrid(_h_yZ, histoDir(), config_appl);
+      _appl_xs = MCgrid::bookGrid(_h_xs, histoDir(), config_appl);
+#endif
+
+#if USE_FNLO
+	  MSG_INFO("Using fastnlo");
+      const string steeringFileName = "MCgrid_CMS_2015_Zee.str";
+      MCgrid::fastnloGridArch arch_fnlo(50, 1, "Lagrange", "OneNode", "sqrtlog10", "linear");
+      MCgrid::fastnloConfig config_fnlo(0, 1960.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_ANTIPROTON, steeringFileName, arch_fnlo);
+      _fnlo_yZ = MCgrid::bookGrid(_h_yZ, histoDir(), config_fnlo);
+      _fnlo_xs = MCgrid::bookGrid(_h_xs, histoDir(), config_fnlo);
+#endif
+    }
+
+
+    /// Perform the per-event analysis
+    void analyze(const Event& event) {
+      
+      // Handle APPL event
+      MCgrid::PDFHandler::HandleEvent(event);
+      
+      const double weight = event.weight();
+
+      const ZFinder& zfinder = applyProjection<ZFinder>(event, "ZFinder");
+      if (zfinder.bosons().size() == 1) {
+        double yZ = fabs(zfinder.bosons()[0].momentum().rapidity());
+        _h_yZ->fill(yZ, weight);
+        _h_xs->fill(1960.0, weight);
+        
+#if USE_APPL
+        _appl_yZ->fill(yZ,event);
+        _appl_xs->fill(1960.0,event);
+#endif
+#if USE_FNLO
+        _fnlo_yZ->fill(yZ,event);
+        _fnlo_xs->fill(1960.0,event);
+#endif
+
+      }
+      else {
+        MSG_DEBUG("no unique lepton pair found.");
+      }
+
+    }
+
+
+    /// Normalise histograms etc., after the run
+    void finalize() {
+      scale(_h_xs, crossSection()/sumOfWeights());
+
+      // Data seems to have been normalized for the avg of the two sides
+      // (+ve & -ve rapidity) rather than the sum, hence the 0.5:
+      scale(_h_yZ, 0.5*crossSection()/sumOfWeights());
+
+#if USE_APPL
+      _appl_yZ->scale(0.5*crossSection()/sumOfWeights());
+      _appl_xs->scale(crossSection()/sumOfWeights());
+      _appl_yZ->exportgrid();
+      _appl_xs->exportgrid();
+#endif
+#if USE_FNLO
+      _fnlo_yZ->scale(0.5*crossSection()/sumOfWeights());
+      _fnlo_xs->scale(crossSection()/sumOfWeights());
+      _fnlo_yZ->exportgrid();
+      _fnlo_xs->exportgrid();
+#endif
+      
+      // Clear event handler
+      MCgrid::PDFHandler::ClearHandler();
+    }
+
+    //@}
+
+
+  private:
+
+    /// @name Histograms
+    //@{
+    Histo1DPtr _h_yZ;
+    Histo1DPtr _h_xs;
+    //@}
+    
+    // Grids
+#if USE_APPL
+    MCgrid::gridPtr _appl_yZ;
+    MCgrid::gridPtr _appl_xs;
+#endif
+#if USE_FNLO
+    MCgrid::gridPtr _fnlo_yZ;
+    MCgrid::gridPtr _fnlo_xs;
+#endif
+
+  };
+
+
+
+  // The hook for the plugin system
+  DECLARE_RIVET_PLUGIN(MCgrid_CMS_2015_Zee);
+
+}
---
+++ MCgrid_CMS_2015_Zee.plot
@@ -0,0 +1,8 @@
+# BEGIN PLOT /MCGRID_CMS_2015_Zee/d01-x01-y01
+#Title=[Uncomment and insert title for histogram d01-x01-y01 here]
+#XLabel=[Uncomment and insert x-axis label for histogram d01-x01-y01 here]
+#YLabel=[Uncomment and insert y-axis label for histogram d01-x01-y01 here]
+# + any additional plot settings you might like, see make-plots documentation
+# END PLOT
+
+# ... add more histograms as you need them ...
