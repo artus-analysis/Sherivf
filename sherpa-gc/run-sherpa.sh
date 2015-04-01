9367612866cef289a8009763b19432c9c2f32604
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-01 18:05:50 +0200

    gc configs: include sherpa in batch mode

--- sherpa-gc/run-sherpa.sh
+++ sherpa-gc/run-sherpa.sh
@@ -1,8 +1,18 @@
-#cd /portal/ekpcms6/home/dhaitz/qcd/SHERPA-MC-2.1.1/Examples/V_plus_Jets/LHC_ZJets
-#cd -
+echo "Make fifo"
+#mkfifo fifo.hepmc
+
+# ini
 export LD_LIBRARY_PATH=/usr/users/dhaitz/local/lib:/usr/users/dhaitz/local/lib64:$LD_LIBRARY_PATH
+export RIVET_ANALYSIS_PATH=$PWD:/usr/users/dhaitz/home/qcd/work:$RIVET_ANALYSIS_PATH
+. /usr/users/dhaitz/home/qcd/ini.sh
 
-echo $PWD
-/usr/users/dhaitz/local/bin/Sherpa RESULT_DIRECTORY=$PWD -f /portal/ekpcms6/home/dhaitz/qcd/SHERPA-MC-2.1.1/Examples/V_plus_Jets/LHC_ZJets/Run.dat -R @SEED_j@
-#sherpa_zjet.hepmc
+export SHERPACMD="/usr/users/dhaitz/local/bin/Sherpa RESULT_DIRECTORY=$PWD EVENTS=1 EVENT_OUTPUT=HepMC_Short[fifo] -R @SEED_0@"
+echo "Start Sherpa: $SHERPACMD"
+$SHERPACMD
 
+export RIVETCMD="rivet  --analysis MCgrid_CMS_2015_Zee fifo.hepmc --pwd"
+echo "start rivet "$RIVETCMD
+$RIVETCMD
+
+echo "PWD: "$PWD
+ls
