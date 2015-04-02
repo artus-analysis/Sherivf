60d945bb17bb7fc9e6ecae39805414c42c1accaa
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-02 15:23:37 +0200

    adapt gc scripts: use on sgs

--- sherpa-gc/run-sherpa.sh
+++ sherpa-gc/run-sherpa.sh
@@ -4,12 +4,13 @@ mkfifo fifo.hepmc
 # ini
 export LD_LIBRARY_PATH=/usr/users/dhaitz/local/lib:/usr/users/dhaitz/local/lib64:$LD_LIBRARY_PATH
 export RIVET_ANALYSIS_PATH=$PWD:/usr/users/dhaitz/home/qcd/work:$RIVET_ANALYSIS_PATH
-. /usr/users/dhaitz/home/qcd/ini.sh
+. ini.sh
 
 export SHERPACMD="/usr/users/dhaitz/local/bin/Sherpa RESULT_DIRECTORY=$PWD EVENTS=@NEVENTS@ EVENT_OUTPUT=HepMC_Short[fifo] -R @SEED_0@"
 echo "Start Sherpa: $SHERPACMD"
 $SHERPACMD &
 
-export RIVETCMD="rivet  --analysis MCgrid_CMS_2015_Zee fifo.hepmc --pwd"
+export RIVETCMD="/usr/users/dhaitz/local/bin/rivet --pwd --analysis MCgrid_CMS_2015_Zee fifo.hepmc"
 echo "Start Rivet "$RIVETCMD
 $RIVETCMD
+
