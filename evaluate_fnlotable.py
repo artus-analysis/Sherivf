ccf90e85908125ab021ae3458494c4530331d814
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-04-06 10:03:42 +0200

    also calculate PDF errors

--- evaluate_fnlotable.py
+++ evaluate_fnlotable.py
@@ -22,7 +22,10 @@ from ROOT import TFile
 def main(
 		n=0,
 		input_filename='fnlo_yZ.txt',
-		pdf_set='../NNPDF21_100.LHgrid',#'CT10nlo.LHgrid',
+		pdf_set=(
+			#'../NNPDF21_100.LHgrid'
+			'CT10nlo.LHgrid'
+		),
 		lumi = 19789,  # in 1/pb
 	):
 
@@ -30,6 +33,7 @@ def main(
 	fnlo = fastNLOLHAPDF(input_filename)
 	fnlo.SetLHAPDFFilename(pdf_set)
 	fnlo.SetLHAPDFMember(n)
+	fnlo.CalcCrossSection()
 	output_filename = input_filename.replace(".txt", ".root")
 
 	print "PDF member:", n, "  output_filename:", output_filename
@@ -41,14 +45,32 @@ def main(
 
 	# fill values
 	xs = np.array(fnlo.GetCrossSection())
-	print xs
 	xs[xs <= 0.] = 0.  # ?
 	for i in range(0, fnlo.GetNDim0Bins()):
 		histo.SetBinContent(i+1, lumi*xs[i])  # multiply with lumi to get event count
 
+	# errors for PDF variations
+	print "Calulating errors for {} PDF variations".format(fnlo.GetNPDFMembers() - 1)
+	errors = [0.] * len(x_binning) #array('d', x_binning)
+	for i in range(1, fnlo.GetNPDFMembers()):
+		fnlo.SetLHAPDFMember(i)
+		fnlo.CalcCrossSection()
+		xsec = fnlo.GetCrossSection()
+		for j in range(len(xsec)):
+			errors[j] += ((xsec[j]-xs[j])/xs[j])**2 # sum up errors in QUADRATURE
+	for i, quad_error in enumerate(errors):
+		errors[i] = math.sqrt(quad_error) # root of squared errors
+
+	# put errors in graph
+	pdf_uncertainty = ROOT.TGraph()
+	pdf_uncertainty.SetName("relative_uncertainty")
+	for i, error in enumerate(errors):
+		pdf_uncertainty.SetPoint(i, x_binning[i], error)
+
 	# save
-	out = TFile(output_filename, "RECREATE")
+	out = TFile(output_filename+"test", "RECREATE")
 	histo.Write()
+	pdf_uncertainty.Write()
 	print "histogram written to file", output_filename
  
 if __name__ == '__main__':
