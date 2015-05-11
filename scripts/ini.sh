c84ee87722e8e377b24343c4ba3b496f846bba5e
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-11 16:24:05 +0200

    ini

--- scripts/ini.sh
+++ scripts/ini.sh
@@ -1,4 +1,4 @@
-export SHERIVFDIR=$(dirname $(readlink -mf ${BASH_SOURCE[0]}))
+export SHERIVFDIR=$(dirname $(dirname $(readlink -mf ${BASH_SOURCE[0]})))
 
 export PATH=$SHERIVFDIR/../Rivet-2.2.0/bin/:$SHERIVFDIR/scripts:$PATH:/usr/users/dhaitz/local/bin
 . $SHERIVFDIR/../Rivet-2.2.0/rivetenv.sh
@@ -17,6 +17,9 @@ export RIVET_ANALYSIS_PATH=$PWD:$RIVET_ANALYSIS_PATH
 #fastnlo 
 # ./configure --prefix=$HOME/local --enable-pyext  PYTHON_VERSION="2.6"
 
+# HERAfitter
+#./configure --enable-lhapdf  LDFLAGS="-L/usr/users/dhaitz/local/lib/ -lblas -llapack -ltmglib"
+
 #yoda
 export PYTHONPATH=$HOME/local/lib64/python2.6/site-packages:$HOME/local/lib/python2.6/site-packages:$PYTHONPATH
 
@@ -34,5 +37,6 @@ export PATH=$SHERIVFDIR/../lhapdf-5.9.1/bin/:$PATH
 # aliases
 alias rivbuild="rivet-buildplugin RivetMyAnalyses.so MCgrid_CMS_2015_Zee.cc -Wl,--export-dynamic,-z,defs  $(pkg-config mcgrid --cflags) $(pkg-config mcgrid --libs)  -lHepMC -lYODA  -DUSE_FNLO=1"
 alias riv="rivet  --analysis MCgrid_CMS_2015_Zee ../SHERPA-MC-2.1.1/Examples/V_plus_Jets/LHC_ZJets/sherpa_zjet.hepmc --pwd"
+
 
 
