7d1d107f99cad186a7ae34c980e259d699458d7f
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-12 13:42:45 +0200

    update GC configs, add NAF functionality

--- sherpa-gc/run-sherpa.sh
+++ sherpa-gc/run-sherpa.sh
@@ -1,16 +1,16 @@
 echo "Make fifo"
 mkfifo fifo.hepmc
+echo @HOME@ @LOCAL@
 
 # ini
-export LD_LIBRARY_PATH=$PWD:/usr/users/dhaitz/local/lib:/usr/users/dhaitz/local/lib64:$LD_LIBRARY_PATH
-export RIVET_ANALYSIS_PATH=$PWD:/usr/users/dhaitz/home/qcd/work:$RIVET_ANALYSIS_PATH
+export LD_LIBRARY_PATH=$PWD:@LOCAL@/lib:@LOCAL@/lib64:$LD_LIBRARY_PATH
+export RIVET_ANALYSIS_PATH=$PWD:@HOME@/sherivf/work:$RIVET_ANALYSIS_PATH
 . ini.sh
 
-export SHERPACMD="/usr/users/dhaitz/local/bin/Sherpa RESULT_DIRECTORY=$PWD EVENTS=@NEVENTS@ EVENT_OUTPUT=HepMC_Short[fifo] -R @SEED_0@"
+export SHERPACMD="@LOCAL@/bin/Sherpa RESULT_DIRECTORY=$PWD EVENTS=@NEVENTS@ EVENT_OUTPUT=HepMC_Short[fifo] -R @SEED_0@"
 echo "Start Sherpa: $SHERPACMD"
 $SHERPACMD &
 
-export RIVETCMD="/usr/users/dhaitz/local/bin/rivet --pwd --analysis MCgrid_CMS_2015_Zee fifo.hepmc"
+export RIVETCMD="@LOCAL@/bin/rivet --pwd --analysis MCgrid_CMS_2015_Zee fifo.hepmc"
 echo "Start Rivet "$RIVETCMD
 $RIVETCMD
-
