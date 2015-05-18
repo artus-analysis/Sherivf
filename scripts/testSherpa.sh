0a02d1afeee833ff5694dd5d2fdb6ac1de7f8e23
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-18 13:48:29 +0200

    testsherpa

--- scripts/testSherpa.sh
+++ scripts/testSherpa.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-TESTDIR=test_$(date +"%Y-%m-%d_%H-%M-%S")
+TESTDIR=$(date +"%Y-%m-%d_%H-%M-%S")
 echo "Make directory $TESTDIR"
-mkdir $TESTDIR
-cd $TESTDIR
+mkdir -p $SHERIVFDIR/test/$TESTDIR
+cd $SHERIVFDIR/test/$TESTDIR
 cp -r $SHERIVFDIR/sherpa-cfg/* .
 
 echo "Make fifo"
 #mkfifo fifo.hepmc
 
-export SHERPACMD="Sherpa RESULT_DIRECTORY=$PWD EVENTS=1 EVENT_OUTPUT=HepMC_Short[fifo]"
+export SHERPACMD="/usr/users/dhaitz/local/bin/Sherpa RESULT_DIRECTORY=$PWD EVENTS=1 EVENT_OUTPUT=HepMC_Short[fifo]"
 echo "Start Sherpa: $SHERPACMD"
-#$SHERPACMD &
-$SHERPACMD
+$SHERPACMD #&
 
-export RIVETCMD="rivet --pwd --analysis MCgrid_CMS_2015_Zee fifo.hepmc"
+export RIVETCMD="/usr/users/dhaitz/local/bin/rivet --pwd --analysis MCgrid_CMS_2015_Zee fifo.hepmc"
 echo "Start Rivet "$RIVETCMD
 #$RIVETCMD
 
