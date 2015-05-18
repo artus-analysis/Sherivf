5f8d1470447892baa24ad6d334ebe93b5898b49a
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-18 13:52:54 +0200

    gc cfgs

--- sherpa-gc/run-sherpa.sh
+++ sherpa-gc/run-sherpa.sh
@@ -4,6 +4,7 @@ mkfifo fifo.hepmc
 # ini
 export LD_LIBRARY_PATH=$PWD:@LOCAL@/lib:@LOCAL@/lib64:$LD_LIBRARY_PATH
 export RIVET_ANALYSIS_PATH=$PWD:@HOME@/sherivf/work:$RIVET_ANALYSIS_PATH
+cp -r $SHERIVFDIR/sherpa-cfg/Process .
 . ini.sh
 
 export SHERPACMD="@LOCAL@/bin/Sherpa RESULT_DIRECTORY=$PWD EVENTS=@NEVENTS@ EVENT_OUTPUT=HepMC_Short[fifo] -R @SEED_0@"
