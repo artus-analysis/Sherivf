0e9576014fb3e1fd1335f91d1ce8b297a4f380b1
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-05-21 13:40:54 +0200

    updated run_sherpa

--- sherpa-gc/run-sherpa.sh
+++ sherpa-gc/run-sherpa.sh
@@ -1,16 +1,28 @@
 echo "Make fifo"
-mkfifo fifo.hepmc
+#mkfifo fifo.hepmc
+
+echo "SHERIVFDIR: "
 
 # ini
 export LD_LIBRARY_PATH=$PWD:@LOCAL@/lib:@LOCAL@/lib64:$LD_LIBRARY_PATH
 export RIVET_ANALYSIS_PATH=$PWD:@HOME@/sherivf/work:$RIVET_ANALYSIS_PATH
-cp -r $SHERIVFDIR/sherpa-cfg/Process .
 . ini.sh
+cp -r $SHERIVFDIR/sherpa-cfg/* .
+#cp $SHERIVFDIR/fnlo_pTZ_warmup.txt $SHERIVFDIR/fnlo_yZ_warmup.txt $SHERIVFDIR/MCgrid_CMS_2015_Zee.cc $SHERIVFDIR/MCgrid_CMS_2015_Zee.str $SHERIVFDIR/MCgrid_CMS_2015_Zee_2.str .
+cp $SHERIVFDIR/MCgrid_CMS_2015_Zee.cc $SHERIVFDIR/MCgrid_CMS_2015_Zee.str $SHERIVFDIR/MCgrid_CMS_2015_Zee_2.str .
+cp $SHERIVFDIR/RivetMyAnalyses.so .
 
-export SHERPACMD="@LOCAL@/bin/Sherpa RESULT_DIRECTORY=$PWD EVENTS=@NEVENTS@ EVENT_OUTPUT=HepMC_Short[fifo] -R @SEED_0@"
+
+ls
+echo ""
+echo ""
+echo ""
+ls Process/*
+
+export SHERPACMD="@LOCAL@/bin/Sherpa EVENTS=@NEVENTS@"
 echo "Start Sherpa: $SHERPACMD"
-$SHERPACMD &
+$SHERPACMD # &
 
-export RIVETCMD="@LOCAL@/bin/rivet --pwd --analysis MCgrid_CMS_2015_Zee fifo.hepmc"
-echo "Start Rivet "$RIVETCMD
-$RIVETCMD
+#export RIVETCMD="@LOCAL@/bin/rivet --pwd --analysis MCgrid_CMS_2015_Zee fifo.hepmc"
+#echo "Start Rivet "$RIVETCMD
+#$RIVETCMD
