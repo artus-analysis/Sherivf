1f124fd7ef52ad5eaf1b26c3035a7a850eb2c826
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-06-04 12:34:23 +0200

    fix

--- scripts/testSherpa.sh
+++ scripts/testSherpa.sh
@@ -9,9 +9,8 @@ cd $SHERIVFDIR/test/$TESTDIR
 
 # copy all cfg files
 cp -r $SHERIVFDIR/sherpa-cfg/* .
-cp $SHERIVFDIR/fnlo-cfg/fnlo_*_warmup.txt $SHERIVFDIR/fnlo_yZ_warmup.txt .
-cp $SHERIVFDIR/MCgrid_CMS_2015_Zee.cc $SHERIVFDIR/MCgrid_CMS_2015_Zee.str $SHERIVFDIR/MCgrid_CMS_2015_Zee_2.str .
-cp $SHERIVFDIR/MCgrid_CMS_2015_Zee.cc $SHERIVFDIR/MCgrid_CMS_2015_Zee.str $SHERIVFDIR/MCgrid_CMS_2015_Zee_2.str .
+#cp $SHERIVFDIR/fnlo-cfg/fnlo_*_warmup.txt .
+cp $SHERIVFDIR/MCgrid_CMS_2015_Zee*.* .
 cp $SHERIVFDIR/RivetMyAnalyses.so .
 
 export SHERPACMD="/usr/users/dhaitz/local/bin/Sherpa EVENTS=${1:-1000}"
