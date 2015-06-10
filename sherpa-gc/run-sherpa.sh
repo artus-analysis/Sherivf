fe30aa35e3ce2beeddf90dacbb8f2e4376a161cf
Dominik Haitz <dhaitz@ekp.uni-karlsruhe.de>
2015-06-10 12:51:54 +0200

    debug output

--- sherpa-gc/run-sherpa.sh
+++ sherpa-gc/run-sherpa.sh
@@ -2,12 +2,18 @@
 export LD_LIBRARY_PATH=$PWD:@LOCAL@/lib:@LOCAL@/lib64:$LD_LIBRARY_PATH
 export RIVET_ANALYSIS_PATH=$PWD:@HOME@/sherivf/work:$RIVET_ANALYSIS_PATH
 . ini.sh
+
+echo "SHERIVFDIR: "$SHERIVFDIR
 cp -r $SHERIVFDIR/sherpa-cfg/* .
 cp $SHERIVFDIR/MCgrid_CMS_2015_Zee.cc $SHERIVFDIR/MCgrid_CMS_2015_Zee*.str .
 cp $SHERIVFDIR/RivetMyAnalyses.so .
 
 @WARMUP@
 
+echo -e "\ndirectory contents:"
+ls
+echo "\n\n"
+
 export SHERPACMD="@LOCAL@/bin/Sherpa -e @NEVENTS@ -R @SEED_0@"
-echo "Start Sherpa: $SHERPACMD"
+echo -e "Start Sherpa: $SHERPACMD\n"
 $SHERPACMD
