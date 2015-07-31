d5e9c215533fe1bf1fa8eadc5eb5747f23ea2a02
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-07-31 10:27:45 +0200

    make pdf plots

--- harryplotter/pdf.py
+++ harryplotter/pdf.py
@@ -45,6 +45,44 @@ def nnpdf(args=None):
 		'labels': ['original', 'reweighted'],
 		'www': 'nnpdf'
 	})
+	
+def pdfs_thesis(args=None, additional_dictionary=None):
+	"""PDF plots for thesis"""
+	plots = []
+	flavours = ['gluon', 'd_quark', 'u_quark', 'd_antiquark', 'u_antiquark']
+	labels = ["gluon/10", "Down", "Up", "Anti-Down", "Anti-Up"]
+	for q, upper_y_lim in zip(
+		[1.9, 3, 10, 14, 91.2, 200],
+		[3]*10,#[0.8, 1.2, 2, 2, 3.5, 4]
+	):
+		d = {
+			"files": ["pdf_sets/pdfs_for_plotting_{}.root".format(str(q).replace(".", "_"))],
+			"folders": [""],
+			'x_expressions': flavours,
+			"nicks": flavours,
+			
+			"analysis_modules": ["ScaleHistograms", "ConvertToTGraphErrors"],
+			"scale_nicks": ["gluon"],
+			"scale": 0.1,
+			
+			"line_styles": ["-"],
+			"markers": ["fill"],
+			"x_label": r"$\\mathit{x}$",
+			"x_log": True,
+			"labels": labels,
+			"y_label": "PDF xfx",
+			"x_lims": [1e-4, 1],
+			"y_lims": [0, upper_y_lim],
+			"texts": [r"NNPDF 2.3 NLO\n$\\mathit{Q}=$" + " {} GeV".format(q)],
+			"texts_x": [0.05],
+			
+			"filename": "pdf_{}".format(("%03d" % q).replace(".", "_")),
+		}
+
+		if additional_dictionary is not None:
+			d.update(additional_dictionary)
+		plots.append(d)
+	harryinterface.harry_interface(plots, args)
 
 
 if __name__ == '__main__':
