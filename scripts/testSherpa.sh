373cfae9378ac834aff6f41b90f8c215d2c0d6ce
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-08-04 09:51:17 +0200

    test

--- scripts/testSherpa.sh
+++ scripts/testSherpa.sh
@@ -19,14 +19,10 @@ $SHERPACMD
 
 echo $SHERIVFDIR/test/$TESTDIR
 
-export MERLINCMD="merlin.py --live evince --input-module InputYoda -i $SHERIVFDIR/test/$TESTDIR/Rivet.yoda"
-
 export MERLINCMD_BASE="merlin.py --live evince --input-module  InputRootZJet InputYoda --yodafiles $SHERIVFDIR/test/$TESTDIR/Rivet.yoda -i /usr/users/dhaitz/home/artus/Excalibur/old_work/data_ee_corr.root --folder all_AK5PFJetsCHSL1L2L3Res --nicks data --analys NormalizeToFirstHisto  --y-errors 0"
-
 export MERLINCMD="${MERLINCMD_BASE} -x zpt --x-bins \"25,0,250\" --nicks-whitelist data d01"
-echo $MERLINCMD
-
+#echo $MERLINCMD
 export MERLINCMD="${MERLINCMD_BASE} -x \"abs(zy)\" --x-bins \"25,0,2.5\" --nicks-whitelist data d02"
-echo $MERLINCMD
+#echo $MERLINCMD
 
-$MERLINCMD
+#$MERLINCMD
