0f84702c3f37984f83eeb4643e936aabe54fe6b5
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-08-05 16:39:10 +0200

    no plotting for tests

--- scripts/testSherpa.sh
+++ scripts/testSherpa.sh
@@ -19,10 +19,3 @@ $SHERPACMD
 
 echo $SHERIVFDIR/test/$TESTDIR
 
-export MERLINCMD_BASE="merlin.py --live evince --input-module  InputRootZJet InputYoda --yodafiles $SHERIVFDIR/test/$TESTDIR/Rivet.yoda -i /usr/users/dhaitz/home/artus/Excalibur/old_work/data_ee_corr.root --folder all_AK5PFJetsCHSL1L2L3Res --nicks data --analys NormalizeToFirstHisto  --y-errors 0"
-export MERLINCMD="${MERLINCMD_BASE} -x zpt --x-bins \"25,0,250\" --nicks-whitelist data d01"
-#echo $MERLINCMD
-export MERLINCMD="${MERLINCMD_BASE} -x \"abs(zy)\" --x-bins \"25,0,2.5\" --nicks-whitelist data d02"
-#echo $MERLINCMD
-
-#$MERLINCMD
