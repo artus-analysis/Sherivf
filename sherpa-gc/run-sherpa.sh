93956e2d585e01980845985d031e14a3982e7ecf
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-08-28 09:36:20 +0200

    update gc cfg

--- sherpa-gc/run-sherpa.sh
+++ sherpa-gc/run-sherpa.sh
@@ -1,17 +1,16 @@
-# ini
+#!/bin/bash
+
+echo -e "\ndirectory contents:"
+ls
+
 export LD_LIBRARY_PATH=$PWD:@LOCAL@/lib:@LOCAL@/lib64:$LD_LIBRARY_PATH
 export RIVET_ANALYSIS_PATH=$PWD:@HOME@/sherivf/work:$RIVET_ANALYSIS_PATH
 . ini.sh
 
 echo "SHERIVFDIR: "$SHERIVFDIR
-cp -r $SHERIVFDIR/sherpa-cfg/* .
-cp $SHERIVFDIR/MCgrid_CMS_2015_Zee.cc $SHERIVFDIR/MCgrid_CMS_2015_Zee*.str .
-cp $SHERIVFDIR/RivetMyAnalyses.so .
 
 @WARMUP@
 
-echo -e "\ndirectory contents:"
-ls
 echo -e "\n\n"
 
 export SHERPACMD="@LOCAL@/bin/Sherpa -e @NEVENTS@ -R @SEED_0@"
