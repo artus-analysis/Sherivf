d88d91fb183dae93dab79c25c8af2b9645951835
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-08-31 17:24:40 +0200

    rivet: switch lo order

--- fnlo-cfg/MCgrid_CMS_2015_Zee.cc
+++ fnlo-cfg/MCgrid_CMS_2015_Zee.cc
@@ -72,10 +72,10 @@ public:
 
 	MSG_INFO("Creating fastnloGridArch and fastnloConfig");
 	MCgrid::fastnloGridArch arch_fnlo(50, 1, "Lagrange", "OneNode", "sqrtlog10", "linear");
-	MCgrid::fastnloConfig config_fnlo(0, 8000.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON, steeringFileName, arch_fnlo);
-	MCgrid::fastnloConfig config_fnlo_2(0, 8000.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON, steeringFileName2, arch_fnlo);
-	MCgrid::fastnloConfig config_fnlo_3(0, 8000.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON, steeringFileName3, arch_fnlo);
-	MCgrid::fastnloConfig config_fnlo_4(0, 8000.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON, steeringFileName4, arch_fnlo);
+	MCgrid::fastnloConfig config_fnlo(1, 8000.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON, steeringFileName, arch_fnlo);
+	MCgrid::fastnloConfig config_fnlo_2(1, 8000.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON, steeringFileName2, arch_fnlo);
+	MCgrid::fastnloConfig config_fnlo_3(1, 8000.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON, steeringFileName3, arch_fnlo);
+	MCgrid::fastnloConfig config_fnlo_4(1, 8000.0, MCgrid::BEAM_PROTON, MCgrid::BEAM_PROTON, steeringFileName4, arch_fnlo);
 
 	MSG_INFO("bookGrid for yZ. histoDir: " << histoDir());
 	_fnlo_pTZ = MCgrid::bookGrid(_h_pTZ, histoDir(), config_fnlo, "fnlo_pTZ_warmup.tab");
