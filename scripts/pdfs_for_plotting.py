1175d3dbec3a0f7b121a83aee2fe4f470c4c8bd3
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-09-02 11:55:50 +0200

    more flaovurs in pdf root files

--- scripts/pdfs_for_plotting.py
+++ scripts/pdfs_for_plotting.py
@@ -6,7 +6,7 @@ import pdf_2_root
 for q in [1, 1.9, 3, 10, 14, 91.2, 200]:
 	pdf_2_root.main(
 		'NNPDF30_nlo_as_0118',
-		[0, 1, 2, -1, -2],
+		[0, 1, 2, -1, -2, 7, 8, 3, -3],
 		"pdfs_for_plotting_{}.root".format(str(q).replace(".", "_")),
 		100,
 		q,
