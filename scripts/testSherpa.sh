35988f342e2f35abc501b1f13887362c30ccb1ec
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-09-08 21:00:36 +0200

    testscript

--- scripts/testSherpa.sh
+++ scripts/testSherpa.sh
@@ -2,20 +2,33 @@
 
 set -e
 
+
 TESTDIR=$(date +"%Y-%m-%d_%H-%M-%S")
 echo "Make directory $TESTDIR"
 mkdir -p $SHERIVFDIR/test/$TESTDIR
 cd $SHERIVFDIR/test/$TESTDIR
 
+
+export MCGRID_OUTPUT_PATH=$PWD
+export MCGRID_PHASESPACE_PATH=$PWD
+
 # copy all cfg files
 cp -r $SHERIVFDIR/sherpa-cfg/fo/* .
-cp $SHERIVFDIR/fnlo-cfg/fnlo_*_warmup.tab .
 cp $SHERIVFDIR/MCgrid_CMS_2015_Zee*.* .
 cp $SHERIVFDIR/RivetMyAnalyses.so .
+cp $SHERIVFDIR/fnlo-cfg/*.evtcount .
+
+#warmup files
+cp $SHERIVFDIR/fnlo-cfg/*.txt .
+mkdir -p MCgrid_CMS_2015_Zee/phasespace/
+cp *.txt MCgrid_CMS_2015_Zee/phasespace/
+
 
 export SHERPACMD="$HOME/local/bin/Sherpa $@"
 echo "Start Sherpa: $SHERPACMD"
 $SHERPACMD
+
+cp MCgrid_CMS_2015_Zee/*.tab .
 
 echo $SHERIVFDIR/test/$TESTDIR
 
