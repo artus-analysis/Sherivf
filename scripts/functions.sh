38091743428ab849facef27f5902f4bc4948bd4f
dhaitz <dhaitz@ekp.uni-karlsruhe.de>
2015-09-10 18:29:57 +0200

    allplots now python

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -21,34 +21,7 @@ calculate_all_correlations(){
 
 
 # plotting
-export WWWSUBDIR="zee/"
-make_allplots(){
-	merlin.py --py pdfs_thesis --www ${WWWSUBDIR}pdfs
-	merlin.py --py pdf_correlations --www ${WWWSUBDIR}correlations
-
-	#merlin.py --py z_hlt --www ${WWWSUBDIR}z_trigger
-	merlin.py --py electron_id --www ${WWWSUBDIR}electron_id
-	merlin.py --py electron_corr --www ${WWWSUBDIR}momentum_corrections
-	merlin.py --py electron_trigger_sf --www ${WWWSUBDIR}electron_sf
-
-	merlin.py --py zee_bkgrs --no-njets --no-ybins --no-mcs --www ${WWWSUBDIR}backgrounds
-	merlin.py --py emu --www ${WWWSUBDIR}emu
-
-	merlin.py --py different_iterations --www ${WWWSUBDIR}unfolding_iteration
-	merlin.py --py response_matrix --www ${WWWSUBDIR}unfolding_reponsematrices
-	merlin.py --py unfolding_comparison --www ${WWWSUBDIR}unfolding_comparisons
-
-	merlin.py --py sherpa --www ${WWWSUBDIR}sherpa
-	merlin.py --py sherpa_mc --www ${WWWSUBDIR}sherpa_mc
-
-	merlin.py --py fastnlo_pdfsets --www ${WWWSUBDIR}fastnlo_pdfsets
-	merlin.py --py fastnlo_pdfmember --www ${WWWSUBDIR}fastnlo_pdfmember
-	merlin.py --py sherpa_fastnlo --www ${WWWSUBDIR}fastnlo_sherpa
-
-	merlin.py --py nnpdf --www ${WWWSUBDIR}nnpdf
-}
-
-
+alias make_allplots="merlin.py --py allplots"
 
 ## Final PDF fitting
 
