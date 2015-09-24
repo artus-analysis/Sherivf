12273803c54a2cede73c869944ee1be2b0fd7ce9
dhaitz <dhaitz@cern.ch>
2015-09-24 13:54:11 +0200

    fucntions for pdf fit

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -18,9 +18,9 @@ calculate_all_correlations(){
 # plotting
 alias make_allplots="merlin.py --py allplots"
 
-## Final PDF fitting
 
-hera_fit(){
+## Final PDF fitting
+nnpdf_fit(){
 	PDFSET=NNPDF30_nlo_as_0118
 	cd $SHERIVFDIR/../herafitter-1.1.1
 	rm $SHERIVFDIR/../herafitter-1.1.1/output/NNPDF*_HighStat_chi2/* $SHERIVFDIR/../herafitter-1.1.1/NNPDF/data/* -rf
@@ -30,4 +30,34 @@ hera_fit(){
 	pdf_2_root.py -p ${PDFSET}_HighStat_chi2_nRep100 -f 0 1 2 3 4 -1 -2
 	cp ${PDFSET}_HighStat_chi2_nRep100.root $SHERIVFDIR/pdf_sets
 	cd $SHERIVFDIR
+}
+
+hera_fit(){
+	cd $SHERIVFDIR/../herafitter-1.1.1
+	FitPDF
+	migrate
+}
+
+migrate(){
+	install_lhapdf5
+	cd $SHERIVFDIR/../herafitter-1.1.1
+	export LHAPATH=$PWD
+	../creategrids PDFs.LHgrid
+	install_lhapdf6
+	pdf_2_root.py -p PDFs  -f 0 1 2 3 4 -1 -2
+	mv PDFs.root $SHERIVFDIR
+}
+
+
+install_lhapdf(){
+	rm /usr/users/dhaitz/local/lib64/python2.6/site-packages/lhapdf.*
+	cd $SHERIVFDIR/../$1
+	make install
+	cd -
+}
+install_lhapdf6(){
+	install_lhapdf LHAPDF-6.1.5
+}
+install_lhapdf5(){
+	install_lhapdf lhapdf-5.9.1
 }
