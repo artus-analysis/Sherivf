4d75bfdc833b4d75e1990c3abdcbe5b7c59519ab
dhaitz <dhaitz@cern.ch>
2015-10-14 17:34:27 +0200

    CombineTgraphs: generalize

--- harryplotter/modules/combinetgraphs.py
+++ harryplotter/modules/combinetgraphs.py
@@ -16,25 +16,47 @@ class CombineTGraphs(analysisbase.AnalysisBase):
 	def modify_argument_parser(self, parser, args):
 		super(CombineTGraphs, self).modify_argument_parser(parser, args)
 		self.envelope_options = parser.add_argument_group("{} options".format(self.name()))
-		self.envelope_options.add_argument("--combine-nick1", type=str)
-		self.envelope_options.add_argument("--combine-nick2", type=str)
+		self.envelope_options.add_argument("--combine-nicks", type=str, nargs="+")
+		#self.envelope_options.add_argument("--combine-nick2", type=str)
 
 	def run(self, plotData=None):
 		super(CombineTGraphs, self).run(plotData)
 
-		graph_1 = plotData.plotdict["root_objects"][plotData.plotdict["combine_nick1"]]
-		graph_2 = plotData.plotdict["root_objects"][plotData.plotdict["combine_nick2"]]
-		graph = ROOT.TGraphAsymmErrors(graph_1)
-
+		root_objects = [plotData.plotdict["root_objects"][nick] for nick in plotData.plotdict["combine_nicks"]]
+		graph = False
+		for root_object in root_objects:
+			if isinstance(root_object, ROOT.TGraphAsymmErrors):
+				graph = ROOT.TGraphAsymmErrors(root_object)
+		if not graph:
+			if isinstance(root_objects[0], ROOT.TH1):
+				histo = root_objects[0]
+				graph = ROOT.TGraphAsymmErrors(histo.GetNbinsX())
+				for i in range(histo.GetNbinsX()):
+					graph.SetPoint(i, histo.GetBinCenter(i+1), 0)
+					graph.SetPointEXlow(i, histo.GetBinWidth(i+1)/2.)
+					graph.SetPointEXhigh(i, histo.GetBinWidth(i+1)/2.)
+			else:
+				pass #TODO warn
+				
+				
 		for i in range(graph.GetN()):
-			# y errors low/high as difference to the min/max values
-			graph.SetPointEYhigh(i, math.sqrt(graph_1.GetErrorYhigh(i)**2 + graph_2.GetErrorYhigh(i)**2))
-			graph.SetPointEYlow(i, math.sqrt(graph_1.GetErrorYlow(i)**2 + graph_2.GetErrorYlow(i)**2))
-			#print roottools.RootTools.scale_tgraph(graph, i)
-			#print roottools.RootTools.scale_tgraph(graph_1, i)
-			# x errors as half the bin width
-			#graph.SetPointEXhigh(i-1, 0.5 * center_histo.GetBinWidth(i))
-			#graph.SetPointEXlow(i-1, 0.5 * center_histo.GetBinWidth(i))
+			error_low, error_high = 0., 0.
+			for histogram in root_objects:
+				if isinstance(histogram, ROOT.TGraphAsymmErrors):
+					error_high += histogram.GetErrorYhigh(i)**2
+					error_low += histogram.GetErrorYlow(i)**2
+				elif isinstance(histogram, ROOT.TGraph):
+					error_high += histogram.GetErrorY(i)**2
+					error_low += histogram.GetErrorY(i)**2
+				elif isinstance(histogram, ROOT.TH1):
+					error_high += histogram.GetBinError(i+1)**2
+					error_low += histogram.GetBinError(i+1)**2
+					
+			error_high = math.sqrt(error_high)
+			error_low = math.sqrt(error_low)
+			
+			graph.SetPointEYhigh(i, error_high)
+			graph.SetPointEYlow(i, error_low)
 
 		plotData.plotdict["root_objects"]['combined'] = graph
 		plotData.plotdict['nicks'].append('combined')
