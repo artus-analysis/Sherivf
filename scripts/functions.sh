1bfbdd4160ca3a3c8179ff453c1571af2ed531da
dhaitz <dhaitz@cern.ch>
2015-12-03 07:33:34 +0100

    cleanup

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# CMS data analysis steps
+####### CMS data analysis steps
+
+# execute the different steps for data analysis
 make_analysis(){
 	merlin.py --py subtract_backgrounds
 	merlin.py --py unfold --no-mcs
@@ -16,6 +18,7 @@ done
 rm -f $SHERIVFDIR/herafitter/CMS_Zee_HFinput_*.txt
 }
 
+# calculate the root files for the correlation plots
 calculate_all_correlations(){
 	for set in NNPDF30_nlo_as_0118 NNPDF23_nlo_as_0118; do
 		for i in zpt abszy zmass; do
@@ -57,17 +60,10 @@ clear_pdfuncs()
 	rm $SHERIVFDIR/5_pdfunc/*.root
 }
 
+# SHERPA
+sherpa_integrate()
+{
+	cd $SHERIVFDIR/sherpa-cfg/$1
+	Sherpa EVENTS=1000
+}
 
-## for switching LHAPDF versions
-install_lhapdf(){
-	rm /usr/users/dhaitz/local/lib64/python2.6/site-packages/lhapdf.*
-	cd $SHERIVFDIR/../$1
-	make install
-	cd -
-}
-install_lhapdf6(){
-	install_lhapdf LHAPDF-6.1.5
-}
-install_lhapdf5(){
-	install_lhapdf lhapdf-5.9.1
-}
