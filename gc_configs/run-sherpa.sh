371d95d0754b13db4565e6108d0b922295cfdb09
dhaitz <dhaitz@cern.ch>
2015-12-07 13:27:34 +0100

    new gc configs

--- gc_configs/run-sherpa.sh
+++ gc_configs/run-sherpa.sh
@@ -5,7 +5,7 @@ ls -lt
 
 export LD_LIBRARY_PATH=$PWD:@LOCAL@/lib:@LOCAL@/lib64:$LD_LIBRARY_PATH
 export RIVET_ANALYSIS_PATH=$PWD:@HOME@/sherivf/work:$RIVET_ANALYSIS_PATH
-. ini.sh
+#. ini.sh
 export MCGRID_OUTPUT_PATH=$PWD
 export MCGRID_PHASESPACE_PATH=$PWD
 
@@ -13,8 +13,8 @@ export MCGRID_PHASESPACE_PATH=$PWD
 @WARMUP@
 
 # copy warmup files, create folders
-mkdir -p MCgrid_CMS_2015_Zee/phasespace/
-mv *.txt MCgrid_CMS_2015_Zee/phasespace/
+mkdir -p @ANALYSIS@/phasespace/
+mv *.txt @ANALYSIS@/phasespace/
 
 echo "SHERIVFDIR: "$SHERIVFDIR
 
@@ -24,8 +24,8 @@ export SHERPACMD="@SHERPAPATH@/Sherpa -e @NEVENTS@ -R @SEED_0@"
 echo -e "Start Sherpa: $SHERPACMD\n"
 $SHERPACMD
 
-mv MCgrid_CMS_2015_Zee/*.tab .
-mv MCgrid_CMS_2015_Zee/phasespace/*.txt .
+mv @ANALYSIS@/*.tab .
+mv @ANALYSIS@/phasespace/*.txt .
 
 
 echo -e "\nafter sherpa:"
