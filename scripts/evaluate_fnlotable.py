71a38f9a1c74826b29ea88a30b02f8c06fe30517
dhaitz <dhaitz@cern.ch>
2015-12-08 09:56:55 +0100

    fix evaluate table

--- scripts/evaluate_fnlotable.py
+++ scripts/evaluate_fnlotable.py
@@ -41,7 +41,7 @@ def main(
 	print "PDF member:", member, "  output_filename:", output_filename
 
 	# make histo
-	x_binning = sorted(list(set([item for sublist in fnlo.GetDim0BinBoundaries() for item in sublist])))
+	x_binning = sorted(list(set([item for sublist in fnlo.GetDim0BinBounds() for item in sublist])))
 	histo = ROOT.TH1D(str(member),str(member),len(x_binning)-1, array('d', x_binning))
 
 
