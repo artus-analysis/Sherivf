b3ec603df93972e19322876c7e380e08cceff52f
dhaitz <dhaitz@cern.ch>
2016-05-23 11:18:35 +0200

    shell scripts update

--- scripts/ini.sh
+++ scripts/ini.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 export SHERIVFDIR=$(dirname $(dirname $(readlink -mf ${BASH_SOURCE[0]})))
-export HERADIR=$SHERIVFDIR/../xFitter-1.2.0
 
-# additional excalibur cfgs: vary electron scale etc. etc.
-export EXCALIBURCONFIGS=$(dirname $(readlink -mf ${BASH_SOURCE[0]})):$EXCALIBURCONFIGS
-
-if [ -f $SHERIVFDIR/scripts/functions.sh ]; then
-	. $SHERIVFDIR/scripts/functions.sh
-fi
 if [ -f $SHERIVFDIR/scripts/configures.sh ]; then
 	. $SHERIVFDIR/scripts/configures.sh
 fi
@@ -20,16 +13,17 @@ if ( [[ $HOSTNAME == *"naf"* ]] || [[ $HOSTNAME == *"bird"* ]] ); then
 	export SHERIVFDIR=/afs/desy.de/user/d/${USER}/qcd/sherivf
 	export SHERIVF_STORAGE_PATH=/nfs/dust/cms/user/${USER}/sherivf/
 	export PKG_CONFIG_PATH=$SHERIVFDIR/../mcgrid:$PKG_CONFIG_PATH
+	export LOCALHOME=/afs/desy.de/user/d/${USER}/
 else
 	export SHERIVFDIR=/portal/ekpcms6/home/${USER}/qcd/sherivf
 	export SHERIVF_STORAGE_PATH=/storage/a/${USER}/sherivf/
 	export HERA_STORAGE_PATH=/storage/a/${USER}/hera/
 	export PKG_CONFIG_PATH=$SHERIVFDIR/../enrico/mcgrid/mcgrid/:$PKG_CONFIG_PATH
+	export LOCALHOME=/usr/users/${USER}/
 fi
 
 # Rivet
 export PATH=$SHERIVFDIR/../Rivet-2.2.0/bin/:$SHERIVFDIR/scripts:$PATH:/usr/users/${USER}/local/bin
-export PATH=$SHERIVFDIR/../herafitter-1.1.1/bin:$PATH
 export PATH=$HOME/local/bin:$PATH
 export RIVET_ANALYSIS_PATH=$PWD:$RIVET_ANALYSIS_PATH
 if [ -e $SHERIVFDIR/../Rivet-2.2.0/rivetenv.sh ]
@@ -37,17 +31,6 @@ then
 	. $SHERIVFDIR/../Rivet-2.2.0/rivetenv.sh
 fi
 
-#harry plotter
-export PLOTCONFIGS=$SHERIVFDIR/harryplotter:$PYTHONCONFIGS
-export PYTHONPATH=$SHERIVFDIR/harryplotter:$SHERIVFDIR/scripts:$PYTHONPATH
-export MODULES_SEARCH_PATH=$SHERIVFDIR/harryplotter/modules:$MODULES_SEARCH_PATH
-
-
-if ( [[ $HOSTNAME == *"naf"* ]] || [[ $HOSTNAME == *"bird"* ]] ); then
-	export LOCALHOME=/afs/desy.de/user/d/${USER}/
-else
-	export LOCALHOME=/usr/users/${USER}/
-fi
 
 #Sherpa
 export SHERPA_INCLUDE_PATH=$LOCALHOME/local/include/SHERPA-MC
@@ -55,25 +38,14 @@ export SHERPA_SHARE_PATH=$LOCALHOME/local/share/SHERPA-MC
 export SHERPA_LIBRARY_PATH=$LOCALHOME/local/lib/SHERPA-MC
 export LD_LIBRARY_PATH=$SHERPA_LIBRARY_PATH:$LOCALHOME/local/lib:$LD_LIBRARY_PATH
 
-export QCDNUM_ROOT=/portal/ekpcms6/home/${USER}/qcd/qcdnum-17-00-06
 
+#other stuff
+export QCDNUM_ROOT=/portal/ekpcms6/home/${USER}/qcd/qcdnum-17-00-06
 
 export PYTHONPATH=$HOME/local/lib/python2.7/site-packages:$PYTHONPATH
 
-#export BOOSTPATH=$(ls ${VO_CMS_SW_DIR}/${SCRAM_ARCH}/external/boost/* -d | tail -n 1)
 export BOOSTPATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/boost/1.57.0-cflfif
-
-#export BOOSTLIB=${BOOSTPATH}/lib/libboost_regex.so.${BOOSTPATH/*\//}
 export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH:$BOOSTPATH/lib:$HOME/local/lib64/:/usr/users/${USER}/local/lib/SHERPA-MC/
-# -L$(BOOSTPATH)/lib/
-
-#LHAPDF
-if [[ `lhapdf-config  --version` == 5.* ]]; then
-	export LHAPDF_DATA_PATH=/cvmfs/cms.cern.ch/lhapdf/pdfsets/5.9.1
-elif [[ `lhapdf-config  --version` == 6.* ]]; then
-	export LHAPDF_DATA_PATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/lhapdf/6.1.5/share/LHAPDF/
-fi
-export LHAPDF_DATA_PATH=/storage/a/${USER}/PDFsets/:$LHAPDF_DATA_PATH
 
 # aliases
 export RIVET_COMPILER_FLAGS="-std=c++0x -Wl,--export-dynamic,-z,defs  $(pkg-config mcgrid --cflags) $(pkg-config mcgrid --libs)  -lHepMC -lYODA -DUSE_FNLO=1"
