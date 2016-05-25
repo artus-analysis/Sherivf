6f783af1b4907ff66285df9237958a2cb0a3e1ec
dhaitz <dhaitz@cern.ch>
2016-05-25 15:32:51 +0200

    ini and readme

--- README.md
+++ README.md
@@ -19,6 +19,13 @@ sherpa, blackhat
 
 bild mit software and howto
 
+
+First, set up sherivf (to set environment paths and get tools from CVMFS):
+
+    cd SheRivF
+    . scripts/ini_sherivf.sh
+    cd ..
+
 #### BLACKHAT 0.9.9
 * https://blackhat.hepforge.org/trac/wiki/BlackHatInstallation
 * GCC version 4.7.2
@@ -27,23 +34,42 @@ Install in storage:
 
     wget http://www.hepforge.org/archive/blackhat/blackhat-0.9.9.tar.gz
     tar -xzf blackhat-0.9.9.tar.gz
-    ./configure --prefix=/storage/a/${USER}/software/blackhat --with-QDpath=/cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/$QD_CVMFS
-
+    cd blackhat-0.9.9
+    . /cvmfs/cms.cern.ch/$ARCHITECTURE_47/external/gcc/4.7.2/etc/profile.d/init.sh # blackhat has to be compiled with older gcc version
+    ./configure --prefix=/storage/a/${USER}/software/blackhat --with-QDpath=/cvmfs/cms.cern.ch/$ARCHITECTURE_47/external/$QD_CVMFS
+    make -j 12
+    make install
+    . /cvmfs/cms.cern.ch/$ARCHITECTURE/external/gcc/4.9.1-cms/etc/profile.d/init.sh  # return to default compiler
+    cd ..
 #### SHERPA 2.2.0
 https://sherpa.hepforge.org/trac/wiki
 
     wget http://www.hepforge.org/archive/sherpa/SHERPA-MC-2.2.0.tar.gz
-    tar -xvf SHERPA-MC-2.2.0.tar.gz
+    tar -xzf SHERPA-MC-2.2.0.tar.gz
+    cd SHERPA-MC-2.2.0
     ./configure --prefix=$HOME/local  --enable-hepmc2=/cvmfs/cms.cern.ch/$ARCHITECTURE/external/$HEPMC_CVMFS --enable-rivet=/cvmfs/cms.cern.ch/$ARCHITECTURE/external/$RIVET_CVMFS --enable-blackhat=/storage/a/dhaitz/software/blackhat  --enable-fastjet=/cvmfs/cms.cern.ch/$ARCHITECTURE/external/$FASTJET_CVMFS 
+    make -j 12
+    make install
+    cd ..
 
 #### fastNLO 2.3.1pre-2212
     wget http://fastnlo.hepforge.org/code/v23/fastnlo_toolkit-2.3.1pre-2212.tar.gz
+    tar -xzf fastnlo_toolkit-2.3.1pre-2212.tar.gz
+    cd fastnlo_toolkit-2.3.1pre-2212
     ./configure --prefix=$HOME/local --enable-pyext  PYTHON_VERSION='2.6'
+    make -j 8
+    make install
+    cd ..
 
 #### MCgrid 2.0
     wget https://www.hepforge.org/archive/mcgrid/mcgrid-2.0.tar.gz
-    tar -xvf mcgrid-2.0.tar.gz
+    tar -xzf mcgrid-2.0.tar.gz
+    cd mcgrid-2.0
     ./configure --prefix=$HOME/local
+    make -j 8
+    make install
+    cd ..
+
 
 
 
--- scripts/ini_sherivf.sh
+++ scripts/ini_sherivf.sh
@@ -8,27 +8,30 @@ if [ -z $ROOT_INCLUDE_PATH ]; then
 	. $ROOTSYS/bin/thisroot.sh
 fi
 
-# use up-to-date GCC version from CVMFS
-alias set_gcc_472=". /cvmfs/cms.cern.ch/slc6_amd64_gcc472/external/gcc/4.7.2/etc/profile.d/init.sh"
-alias set_gcc_491=". /cvmfs/cms.cern.ch/slc6_amd64_gcc491/external/gcc/4.9.1-cms/etc/profile.d/init.sh"
-
+# use GCC version from CVMFS
+export ARCHITECTURE_47=slc6_amd64_gcc472
+export ARCHITECTURE=slc6_amd64_gcc491
+alias set_gcc_472=". /cvmfs/cms.cern.ch/$ARCHITECTURE_47/external/gcc/4.7.2/etc/profile.d/init.sh"
+alias set_gcc_491=". /cvmfs/cms.cern.ch/$ARCHITECTURE/external/gcc/4.9.1-cms/etc/profile.d/init.sh"
+set_gcc_491
 
 # take software from CVMFS
-export ARCHITECTURE=slc6_amd64_gcc491
 export YODA_CVMFS=yoda/1.3.1
 export HEPMC_CVMFS=hepmc/2.06.07-cms
 export RIVET_CVMFS=rivet/2.2.1-kpegke
 export FASTJET_CVMFS=fastjet/3.1.0
-export QD_CVMFS=qd/2.3.13
 for SOFTWARE in lhapdf/6.1.5 $YODA_CVMFS $HEPMC_CVMFS $FASTJET_CVMFS $RIVET_CVMFS; do
 	. /cvmfs/cms.cern.ch/$ARCHITECTURE/external/$SOFTWARE/etc/profile.d/init.sh
 done
+export QD_CVMFS=qd/2.3.13
+. /cvmfs/cms.cern.ch/$ARCHITECTURE_47/external/$QD_CVMFS/etc/profile.d/init.sh
 
 
+# some paths
 export SHERIVFDIR=$(dirname $(dirname $(readlink -mf ${BASH_SOURCE[0]})))
-
 export PATH=$SHERIVFDIR/scripts:$HOME/local/bin:$PATH
 export LD_LIBRARY_PATH=$HOME/local/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=$HOME/local/lib:$HOME/local/lib/SHERPA-MC:$LD_LIBRARY_PATH
 
 # output paths
 export STORAGE_PATH=/storage/a/${USER}
@@ -36,11 +39,15 @@ export SHERIVF_STORAGE_PATH=$STORAGE_PATH/sherivf/
 export HERA_STORAGE_PATH=$STORAGE_PATH/hera/
 
 
-#export MCGRID_OUTPUT_PATH=$PWD
-#export MCGRID_PHASESPACE_PATH=$PWD
 
 
 export PKG_CONFIG_PATH=/storage/a/dhaitz/software/mcgrid-2.0:$PKG_CONFIG_PATH  #TODO
+
+export RIVET_COMPILER_FLAGS="-std=c++0x -Wl,--export-dynamic,-z,defs $(pkg-config mcgrid --cflags) $(pkg-config mcgrid --libs)  -lYODA"
+
+
+#export MCGRID_OUTPUT_PATH=$PWD
+#export MCGRID_PHASESPACE_PATH=$PWD
 
 # Rivet
 #export PATH=$SHERIVFDIR/../Rivet-2.2.0/bin/:/usr/users/${USER}/local/bin:$PATH
@@ -65,6 +72,5 @@ export PKG_CONFIG_PATH=/storage/a/dhaitz/software/mcgrid-2.0:$PKG_CONFIG_PATH  #
 #export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH:$BOOSTPATH/lib:$HOME/local/lib64/:/usr/users/${USER}/local/lib/SHERPA-MC/
 
 # aliases
-export RIVET_COMPILER_FLAGS="-std=c++0x -Wl,--export-dynamic,-z,defs $(pkg-config mcgrid --cflags) $(pkg-config mcgrid --libs)  -lYODA"
 #    -lHepMC -DUSE_FNLO=1"
 
