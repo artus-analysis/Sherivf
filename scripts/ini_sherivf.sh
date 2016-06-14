05d833d4b15d85be8112ebf10f0eca9ef0aa8fee
dhaitz <dhaitz@cern.ch>
2016-06-14 09:23:56 +0200

    remove obsolete lines in ini script

--- scripts/ini_sherivf.sh
+++ scripts/ini_sherivf.sh
@@ -3,8 +3,6 @@
 # source ROOT if not set
 if [ -z $ROOT_INCLUDE_PATH ]; then
 	export ROOTSYS=/cvmfs/sft.cern.ch/lcg/app/releases/ROOT/5.34.13/x86_64-slc6-gcc48-opt/root/
-	#export GCCLIBS=/cvmfs/sft.cern.ch/lcg/external/gcc/4.7.2/x86_64-slc5/lib64/
-	#export LD_LIBRARY_PATH=$GCCLIBS:$LD_LIBRARY_PATH
 	. $ROOTSYS/bin/thisroot.sh
 fi
 
@@ -43,36 +41,3 @@ export STORAGE_PATH=/storage/a/${USER}
 export SHERIVF_STORAGE_PATH=$STORAGE_PATH/sherivf/
 export HERA_STORAGE_PATH=$STORAGE_PATH/hera/
 export RIVET_COMPILER_FLAGS="-std=c++0x -Wl,--export-dynamic,-z,defs -I/usr/users/$USER/local/include -L/usr/users/${USER}/local/lib -lmcgrid  -lYODA"
-
-
-
-
-
-#export MCGRID_OUTPUT_PATH=$PWD
-#export MCGRID_PHASESPACE_PATH=$PWD
-
-# Rivet
-#export PATH=$SHERIVFDIR/../Rivet-2.2.0/bin/:/usr/users/${USER}/local/bin:$PATH
-#export RIVET_ANALYSIS_PATH=$PWD:$RIVET_ANALYSIS_PATH
-#if [ -e $SHERIVFDIR/../Rivet-2.2.0/rivetenv.sh ]
-#then
-#	. $SHERIVFDIR/../Rivet-2.2.0/rivetenv.sh
-#fi
-
-
-#Sherpa
-#export SHERPA_INCLUDE_PATH=$LOCALHOME/local/include/SHERPA-MC
-#export SHERPA_SHARE_PATH=$LOCALHOME/local/share/SHERPA-MC
-#export SHERPA_LIBRARY_PATH=$LOCALHOME/local/lib/SHERPA-MC
-#export LD_LIBRARY_PATH=$SHERPA_LIBRARY_PATH:$LOCALHOME/local/lib:$LD_LIBRARY_PATH
-
-
-#other stuff
-#export QCDNUM_ROOT=/portal/ekpcms6/home/${USER}/qcd/qcdnum-17-00-06
-#export PYTHONPATH=$HOME/local/lib/python2.7/site-packages:$PYTHONPATH
-#export BOOSTPATH=/cvmfs/cms.cern.ch/slc6_amd64_gcc481/external/boost/1.57.0-cflfif
-#export LD_LIBRARY_PATH=$PWD:$LD_LIBRARY_PATH:$BOOSTPATH/lib:$HOME/local/lib64/:/usr/users/${USER}/local/lib/SHERPA-MC/
-
-# aliases
-#    -lHepMC -DUSE_FNLO=1"
-
